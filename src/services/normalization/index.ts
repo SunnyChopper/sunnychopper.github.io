63656e57e361c84ad5a23171166c7e73dd8698e5
SunnyChopper <ishy.singh@gmail.com>
2026-01-25 23:43:15 -0600

    refactor: remove normalization services
    - Remove dashboard-normalizer and project-normalizer
    - Remove normalization index and test files

--- src/services/normalization/index.ts
+++
@@ -1,3 +0,0 @@
-export { normalizeDashboardSummary } from './dashboard-normalizer';
-export { normalizePriority, normalizeProject } from './project-normalizer';
-export type { ProjectAdapterInput } from './project-normalizer';
